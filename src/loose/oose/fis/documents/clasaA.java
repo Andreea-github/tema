5488fe3593e3a162963b73ea5f3413a368ae3538
Andreea Paris <andreea.steli25@gmail.com>
2020-03-19 22:59:52 +0200

    Clasa A a fost creata

---
+++ src/loose/oose/fis/documents/clasaA.java
@@ -0,0 +1,10 @@
+package loose.oose.fis.documents;
+
+public class clasaA {
+  private  int ani;
+   private String nume;
+    public clasaA(int ani,String nume){
+        this.ani=ani;
+        this.nume=nume;
+    }
+}
